1d33e92742df83fb82a0edb49aac4727c17d37f6
Abdul Hameed <raza2022@gmail.com>
2018-10-18 18:00:44 +0500

    basic SMTP check added

--- server/main.js
+++ server/main.js
@@ -1,5 +1,31 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+    //uncomment which service you are using
+
+    /* SES */
+    process.env.MAIL_URL = `smtps://${encodeURIComponent(Meteor.settings.AWS.SESUser)}:${encodeURIComponent(Meteor.settings.AWS.SESPASSWORD)}@email-smtp.${Meteor.settings.AWS.AWSRegion}.amazonaws.com:465`;
+
+    /* Zoho */
+    // process.env.MAIL_URL = `smtps://${encodeURIComponent(Meteor.settings.zoho.userName)}:${encodeURIComponent(Meteor.settings.zoho.secret)}@smtp.zoho.com:465`;
+    // TODO: add others mailgun etc
+
 });
+
+Meteor.methods({
+    'sendEmail' : () => {
+        // Meteor.defer(function(){
+            console.log( 'Email Sent Start' );
+            Email.send({
+                to: 'to@developer.com',
+                from: 'lazy@developer.com',
+                subject: 'test',
+                text: 'test is always best'
+            });
+            console.log( 'Email Sent end' );
+        // });
+    }
+});
+
+
